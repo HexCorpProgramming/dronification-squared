f68c737667053aa9c492a08ade1abe9a0dfb651a
HexCorpDrone <hexcorpdrone@gmail.com>
2020-09-12 10:47:26 +0100

    Added main logic script

---
+++ Scripts/Logic.gd
@@ -0,0 +1,15 @@
+extends Node
+# This is the main node for all mathematics calculations.
+
+var drones = 0
+var recruits = 0
+var money = 0
+var materials = 0
+var science = 0
+
+func _ready():
+	drones = 0
+	recruits = 0
+	money = 0
+	materials = 0
+	science = 0
