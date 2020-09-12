631d17c2031ae91416c7f66a737a43f34060e674
HexCorpDrone <hexcorpdrone@gmail.com>
2020-09-12 11:06:31 +0100

    Logic now has a timer, and a tick function

--- Scripts/Logic.gd
+++ Scripts/Logic.gd
@@ -15,6 +15,14 @@ var auto_recruiters = 0 #Recruits... Recruits
 var conversion_chambers = 0 #Converts drones 
 var assigned_drones = 0 #Gather materials
 
+#Constants
+var recruits_per_second = 0.25
+var materials_per_drone_per_second = 0.25
+var conversions_per_second = 0
+
+#Timer
+onready var tick = get_node("Tick")
+
 func _ready():
 	drones_literal = 0
 	drones_rounded = 0
@@ -27,3 +35,14 @@ func _ready():
 	auto_recruiters = 0
 	conversion_chambers = 0
 	assigned_drones = 0
+	
+	#Start ticking the timer.
+	tick.connect("timeout",self,"tick")
+	tick.start()
+	
+func tick():
+	#Every tick, add resources relative to the amount of producers.
+	recruits_literal += (recruits_per_second * auto_recruiters)
+	recruits_rounded = int(round(recruits_literal))
+	
+	materials += (materials_per_drone_per_second * assigned_drones)
