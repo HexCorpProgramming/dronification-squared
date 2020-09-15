44841b44bcea1fbab506bbd99badc1f106fc4419
pizzie11 <65429903+pizzie11@users.noreply.github.com>
2020-09-14 23:11:19 -0400

    Main drone display update

--- Menus/DroneCount.gd
+++ Menus/DroneCount.gd
@@ -1,4 +1,7 @@
 extends Label
 
 func _process(_delta):
-	set_text(str(Logic.drones_rounded))
+	var output = str(Logic.drones_rounded)
+	for i in range(output.length()-3,0,-3): #from end of string to beginning
+		output = output.insert(i,",")
+	set_text(output)
