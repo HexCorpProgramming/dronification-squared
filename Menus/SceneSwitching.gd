a4be4a065393ed4ad8f6d168e7fcbaaa168b6088
pizzie11 <65429903+pizzie11@users.noreply.github.com>
2020-10-04 01:28:03 -0400

    Fix SoftLock on credits screen

--- Menus/SceneSwitching.gd
+++ Menus/SceneSwitching.gd
@@ -1,5 +1,6 @@
 extends Control
 
+onready var saveload = load("res://Menus/SaveButtons.gd").new()
 
 func _on_recruitment():
 	visible = false
@@ -31,7 +32,7 @@ func _on_credits():
 	get_node("/root/Root/Credits").visible = true
 	
 func _on_start():
-	get_node("HBoxContainer").save_game()
+	saveload.save_game()
 	visible = false
 	get_node("/root/Root/Header").visible = false
 	get_node("/root/Root/Start").visible = true
